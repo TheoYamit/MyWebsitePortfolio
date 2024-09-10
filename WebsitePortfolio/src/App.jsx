bcc4688bc1139a2eec1f502e519df640246cfb80
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-09-09 18:22:07 -0600

    Update App.jsx

--- WebsitePortfolio/src/App.jsx
+++ WebsitePortfolio/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 function App() {
 
   return (
-    <Router>
+    <Router basename="/MyWebsitePortfolio">
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
