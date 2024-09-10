b24f8cf4e73f94f1fadc467eba2de55c76112309
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-09-09 18:13:40 -0600

    Update vite.config.js

--- WebsitePortfolio/vite.config.js
+++ WebsitePortfolio/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MyWebsitePortfolio/',
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: './',
+    build: {
+      assetsDir: 'assets',
+    },
+  })
